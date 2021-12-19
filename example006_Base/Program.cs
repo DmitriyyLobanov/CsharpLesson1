7cb2d8c3e13fdd3820faff697e42b716ebd17e41
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2021-12-19 16:48:14 +0400

    Example006_Base

---
+++ example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 1;
+int c = 4;
+int d = 8;
+int e = 5;
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.WriteLine($"max = {max}.");
